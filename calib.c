ab50642fa04b2c77190a4a079ae893b1aa41519a
Sparsh Gauba <gaubasparsh@gmail.com>
2019-01-19 02:57:31 -0800

    merged with temp

--- calib.c
+++ calib.c
@@ -1,41 +1,31 @@
-/*
-    This program  reads the angles from the accelerometer and gyroscope
-    on a BerryIMU connected to a Raspberry Pi.
-
-
-    Both the BerryIMUv1 and BerryIMUv2 are supported
-
-    Feel free to do whatever you like with this code
-    Distributed as-is; no warranty is given.
-
-    http://ozzmaker.com/
-*/
-
-
-
 #include <unistd.h>
 #include <math.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <fcntl.h>
 #include <string.h>
 #include <time.h>
-#include "IMU.c"
-//#include "MadgwickAHRS.h"
 #include <sys/time.h>
-#include "MahonyAHRS.h"
+#include "IMU.c"
 
-#define DT 0.006         // [s/loop] loop period in ms
+#define DT 0.005         // [s/loop] loop period in ms
 #define AA 0.97         // complementary filter constant
 
 #define A_GAIN 0.0573    // [deg/LSB]
-#define G_GAIN 0.017     // [deg/s/LSB]
+#define G_GAIN 0.017*2     // [deg/s/LSB] //For 500 deg/s Precision
 #define RAD_TO_DEG 57.29578
 #define M_PI 3.14159265358979323846
 
 #define THRES_A 10
 #define THRES_G 10
+
+// System constants
+#define deltat 0.005f // sampling period in seconds (shown as 20 ms)
+#define gyroMeasError 3.14159265358979f * (5.0f / 180.0f) // gyroscope measurement error in rad/s (shown as 5 deg/s)
+#define beta sqrt(3.0f / 4.0f) * gyroMeasError // compute beta
+// Global system variables
 
 
 ///////////////////////////////////////////////////////
@@ -52,214 +42,294 @@
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
 
+
+float SEq_1 = 1.0f, SEq_2 = 0.0f, SEq_3 = 0.0f, SEq_4 = 0.0f; // estimated orientation quaternion elements with initial conditions
+
+float madAngles[3] = {0.0f};
+
+
+//----------------------------------------------------------------------------------------
+//////////              FUNCTION DECLARATIONS               //////////////////////////////
+//----------------------------------------------------------------------------------------
+
+
+void  INThandler(int sig);// Used to do a nice clean exit when Ctrl-C is pressed
+
+int mymillis();
+
+int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1);
+
+long long * calibrate_acc();
+
+long long *calibrate_gyr();
+
+void filterUpdate(float w_x, float w_y, float w_z, float a_x, float a_y, float a_z);
+
+void computeAngles();
+
+
+int main(int argc, char *argv[])
+{
+  //Raw sensor values
+  int  accRaw[3];
+  int  magRaw[3];
+  int  gyrRaw[3];
+  //Calibrated Acc Values with prev values for filtering;
+  uint64_t ca_x_full = 0;
+  uint64_t ca_y_full = 0;
+  uint64_t ca_z_full = 0;
+  //Calibrated Gyr Values
+  uint64_t cg_x_full = 0;
+  uint64_t cg_y_full = 0;
+  uint64_t cg_z_full = 0;
+
+  int32_t *ca_x = (int32_t *)&ca_x_full;
+  int32_t *ca_y = (int32_t *)&ca_y_full;
+  int32_t *ca_z = (int32_t *)&ca_z_full;
+
+  int32_t *cg_x = (int32_t *)&cg_x_full;
+  int32_t *cg_y = (int32_t *)&cg_y_full;
+  int32_t *cg_z = (int32_t *)&cg_z_full;
+
+  //Values to send to MadgwickAHRS
+  float gyr_rate_rad[3] = {0.0f}; // Converted to Radians per second
+  float acc_G[3] = {0.0f}; // Converted to G
+  float scaledMag[3] = {0.0f}; //After hard iron and soft iron calibration
+
+
+  int startInt;
+  struct  timeval tvBegin, tvEnd,tvDiff;
+
+  signal(SIGINT, INThandler);
+
+  detectIMU();
+  enableIMU();
+
+
+  //Get Calibration offset
+  int64_t *ca = calibrate_acc();
+  int64_t *cg = calibrate_gyr();
+  int G_raw = ca[3];
+  printf("OffAccX: %6lld\tOffAccY: %6lld\tOffAccZ: %6lld\n", ca[0], ca[1], ca[2]);
+  gettimeofday(&tvBegin, NULL);
+  while(1)
+    {
+      startInt  = mymillis();
+      //Move current values to last values;
+      ca_x_full <<= 32;
+      ca_y_full <<= 32;
+      ca_z_full <<= 32;
+      cg_x_full <<= 32;
+      cg_y_full <<= 32;
+      cg_z_full <<= 32;
+
+
+      //Get Raw Values
+      readACC(accRaw);
+      readGYR(gyrRaw);
+      //readMAG(magRaw);
+
+
+      //Subtract offset values;
+      ca_x[0] = (((accRaw[0]-(int)ca[0]) + ca_x[1])/2);
+      ca_y[0] = (((accRaw[1]-(int)ca[1]) + ca_y[1])/2);
+      ca_z[0] = -((accRaw[2] + ca_z[1])/2);
+      cg_x[0] = (((gyrRaw[0]-(int)cg[0]) + cg_x[1])/2);
+      cg_y[0] = (((gyrRaw[1]-(int)cg[1]) + cg_y[1])/2);
+      cg_z[0] = (((gyrRaw[2]-(int)cg[2]) + cg_z[1])/2);
+
+
+      //Convert acc to G's
+      acc_G[0] = ((float)ca_x[0])/((float)G_raw);
+      acc_G[1] = ((float)ca_y[0])/((float)G_raw);
+      acc_G[2] = ((float)ca_z[0])/((float)G_raw);
+
+      //Convert gyr to Rad/s
+      gyr_rate_rad[0] = (float)cg_x[0]  * G_GAIN * M_PI / 180.0f;
+      gyr_rate_rad[1] = (float)cg_y[0]  * G_GAIN * M_PI / 180.0f;
+      gyr_rate_rad[2] = (float)cg_z[0]  * G_GAIN * M_PI / 180.0f;
+
+      //printf("AccX: %4d\tAccY: %4d\tAccZ: %4d\t", ca_x[0], ca_y[0], ca_z[0]);
+
+      //printf("GyrX: %4d\tGyrY: %4d\tGyrZ: %4d\t", cg_x[0], cg_y[0], cg_z[0]);
+
+      //printf("AccX: %5.2f\tAccY: %5.2f\tAccZ: %5.2f\t", acc_G[0], acc_G[1], acc_G[2]);
+
+      filterUpdate(gyr_rate_rad[0], gyr_rate_rad[1], gyr_rate_rad[2], acc_G[0], acc_G[1], acc_G[2]);
+
+      computeAngles();
+        
+      printf("Roll: %5.2f\t Pitch: %5.2f\t Yaw:Z %5.2f", madAngles[0], madAngles[1], madAngles[2]);
+
+      //Each loop should be at least 20ms.
+      while(mymillis() - startInt < (DT*1000))
+        {
+	  usleep(100);
+        }
+
+      printf("Loop Time %d\n", mymillis()- startInt);
+
+    }
+
+}
+
+
 void  INThandler(int sig)// Used to do a nice clean exit when Ctrl-C is pressed
 {
-    signal(sig, SIG_IGN);
-    exit(0);
+  signal(sig, SIG_IGN);
+  exit(0);
 }
 
 int mymillis()
 {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return (tv.tv_sec) * 1000 + (tv.tv_usec)/1000;
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return (tv.tv_sec) * 1000 + (tv.tv_usec)/1000;
 }
 
 int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
 {
-    long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
-    result->tv_sec = diff / 1000000;
-    result->tv_usec = diff % 1000000;
-    return (diff<0);
+  long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
+  result->tv_sec = diff / 1000000;
+  result->tv_usec = diff % 1000000;
+  return (diff<0);
 }
+
+
 
 
 // Returns the offets for raw acc values
 long long * calibrate_acc()
 {
-    long long *ret = malloc(4*sizeof(long long));
-    ret[0] = ret[1] = ret[2] = ret[3] = 0;
-    int accRaw[3];
-    int start = mymillis();
-    for (int i = 0; i < 100; i++)
+  int64_t *ret = malloc(4*sizeof(int64_t));
+  ret[0] = ret[1] = ret[2] = ret[3] = 0;
+  int accRaw[3];
+  int start = mymillis();
+  for (int i = 0; i < 100; i++)
     {
-        readACC(accRaw);
-        ret[0]+=accRaw[0];
-        ret[1]+=accRaw[1];
-        ret[2]+=accRaw[2];
+      readACC(accRaw);
+      ret[0]+=accRaw[0];
+      ret[1]+=accRaw[1];
+      ret[2]+=accRaw[2];
     }
-    printf("*********************************    Loop Time %d     ************************\n", mymillis()- start);
-    ret[0] = ret[0]/100;
-    ret[1] = ret[1]/100;
-    ret[2] = ret[2]/100;
-    //ret[3] is the calculated raw value that corresponds to 1 G
-    ret[3] = (long long)sqrt( pow(ret[0],2) + pow(ret[1],2) + pow(ret[2],2));
-    return ret;
+  printf("*********************************    Loop Time %d     ************************\n", mymillis()- start);
+  ret[0] = ret[0]/100;
+  ret[1] = ret[1]/100;
+  ret[2] = ret[2]/100;
+  //ret[3] is the calculated raw value that corresponds to 1 G
+  ret[3] = (int64_t)sqrt( pow(ret[0],2) + pow(ret[1],2) + pow(ret[2],2));
+  return ret;
 }
+
 
 
 // Returns offests for raw gyr values
 long long *calibrate_gyr()
 {
-    long long *ret = malloc(3*sizeof(long long));
-    ret[0] = ret[1] = ret[2] = 0;
-    int gyrRaw[3];
-    int start = mymillis();
-    for (int i = 0; i < 100; i++)
+  long long *ret = malloc(3*sizeof(long long));
+  ret[0] = ret[1] = ret[2] = 0;
+  int gyrRaw[3];
+  int start = mymillis();
+  for (int i = 0; i < 100; i++)
     {
-        readGYR(gyrRaw);
-        ret[0]+=gyrRaw[0];
-        ret[1]+=gyrRaw[1];
-        ret[2]+=gyrRaw[2];
+      readGYR(gyrRaw);
+      ret[0]+=gyrRaw[0];
+      ret[1]+=gyrRaw[1];
+      ret[2]+=gyrRaw[2];
     }
-    printf("*********************************    Loop Time %d     ************************\n", mymillis()- start);
-    ret[0] = ret[0]/100;
-    ret[1] = ret[1]/100;
-    ret[2] = ret[2]/100;
-    return ret;
+  printf("*********************************    Loop Time %d     ************************\n", mymillis()- start);
+  ret[0] = ret[0]/100;
+  ret[1] = ret[1]/100;
+  ret[2] = ret[2]/100;
+  return ret;
 }
 
-int main(int argc, char *argv[])
+
+
+
+void filterUpdate(float w_x, float w_y, float w_z, float a_x, float a_y, float a_z)
 {
-    //Raw sensor values
-    int  accRaw[3];
-    int  magRaw[3];
-    int  gyrRaw[3];
-
-
-     //Calibrated Acc Values with prev values for filtering;
-    int ca_x[2] = {0};
-    int ca_y[2] = {0};
-    int ca_z[2] = {0};
-
-    //Calibrated Gyr Values
-    int cg_x[2] = {0};
-    int cg_y[2] = {0};
-    int cg_z[2] = {0};
-
-
-    //Values to send to MadgwickAHRS
-    float gyr_rate_rad[3] = {0}; // Converted to Radians per second
-    float acc_G[3] = {0}; // Converted to G
-    float scaledMag[3] = {0}; //After hard iron and soft iron calibration
-
-
-    int startInt  = mymillis();
-    struct  timeval tvBegin, tvEnd,tvDiff;
-
-
-
-
-
-    signal(SIGINT, INThandler);
-
-    detectIMU();
-    enableIMU();
-
-    //Get Calibration offset
-    long long *ca = calibrate_acc();
-    long long *cg = calibrate_gyr();
-
-    int G_raw = (int)ca[3];
-    gettimeofday(&tvBegin, NULL);
-
-
-    while(1)
-    {
-        startInt = mymillis();
-
-        //Slide last values back
-        ca_x[1] = ca_x[0];
-        ca_y[1] = ca_y[0];
-        ca_z[1] = ca_z[0];
-        cg_x[1] = cg_x[0];
-        cg_y[1] = cg_y[0];
-        cg_z[1] = cg_z[0];
-
-
-        //read ACC and GYR data
-        readACC(accRaw);
-        readGYR(gyrRaw);
-        readMAG(magRaw);
-
-        //Subtracted calibration values
-        ca_x[0] = accRaw[0] - ca[0];
-        ca_y[0] = accRaw[1] - ca[1];
-        ca_z[0] = accRaw[2];
-        cg_x[0] = gyrRaw[0] - cg[0];
-        cg_y[0] = gyrRaw[1] - cg[1];
-        cg_z[0] = gyrRaw[2] - cg[2];
-
-        // If new val is less than THRES apart from last, keep last value
-        // Otherwise, new val is the average of the cur and prev
-        ca_x[0] = abs(ca_x[0]) < THRES_A ? 0 : (int)(ca_x[1] + ca_x[0]) / 2;
-        ca_y[0] = abs(ca_y[0]) < THRES_A ? 0 : (int)(ca_y[1] + ca_y[0]) / 2;
-        ca_z[0] = abs(ca_z[0]) < THRES_A ? 0 : (int)(ca_z[1] + ca_z[0]) / 2;
-        cg_x[0] = abs(cg_x[0]) < THRES_G ? 0 : (int)(cg_x[1] + cg_x[0]) / 2;
-        cg_y[0] = abs(cg_y[0]) < THRES_G ? 0 : (int)(cg_y[1] + cg_y[0]) / 2;
-        cg_z[0] = abs(cg_z[0]) < THRES_G ? 0 : (int)(cg_z[1] + cg_z[0]) / 2;
-
-        //Apply hard iron calibration
-        magRaw[0] -= (magXmin + magXmax) /2 ;
-        magRaw[1] -= (magYmin + magYmax) /2 ;
-        magRaw[2] -= (magZmin + magZmax) /2 ;
-
-        //Apply soft iron calibration
-        scaledMag[0]  = (float)(magRaw[0] - magXmin) / (magXmax - magXmin) * 2 - 1;
-        scaledMag[1]  = (float)(magRaw[1] - magYmin) / (magYmax - magYmin) * 2 - 1;
-        scaledMag[2]  = (float)(magRaw[2] - magZmin) / (magZmax - magZmin) * 2 - 1;
-
-        //Convert to G values
-        acc_G[0] = ((float)ca_x[0]/G_raw);
-        acc_G[1] = ((float)ca_y[0]/G_raw);
-        acc_G[2] = ((float)ca_z[0]/G_raw);
-
-        //Convert Gyro raw to RADIANS per second
-        gyr_rate_rad[0] = (float) cg_x[0]  * G_GAIN * M_PI / 180;
-        gyr_rate_rad[1] = (float) cg_y[0]  * G_GAIN * M_PI / 180;
-        gyr_rate_rad[2] = (float) cg_z[0]  * G_GAIN * M_PI / 180;
-
-        //Print Acc Values after Calibration
-        //printf("AccX: %5d\tAccY: %5d\tAccZ: %5d\t", ca_x[0], ca_y[0], ca_z[0]);
-
-        //Print Gyr Values after Calibration
-        //printf("GyrX: %5d\tGyrY: %5d\tGyrZ: %5d\t", cg_x[0], cg_y[0], cg_z[0]);
-
-        //Print Raw Mag Values
-        //printf("MagX: %4d\tMagY: %4d\tMagZ: %4d\t", magRaw[0], magRaw[1], magRaw[2]);
-
-        //Print G values
-        //printf("AccX: %5.2f\tAccY: %5.2f\tAccZ: %5.2f\t", acc_G[0], acc_G[1], acc_G[2]);
-
-        //PRINT GYR DEG PER SEC
-        //printf("GyrX: %5.3f\tGyrY: %5.3f\tGyrZ: %5.3f\t", gyr_rate_rad[0], gyr_rate_rad[1], gyr_rate_rad[2]);
-
-
-
-	//Madgwick Filter with magneto fused
-	//MadgwickAHRSupdate(gyr_rate_rad[0], gyr_rate_rad[1], gyr_rate_rad[2], acc_G[0], acc_G[1], acc_G[2], scaledMag[0], scaledMag[1], scaledMag[2]);
-        //MahonyAHRSupdate(gyr_rate_rad[0], gyr_rate_rad[1], gyr_rate_rad[2], acc_G[0], acc_G[1], acc_G[2], scaledMag[0], scaledMag[1], scaledMag[2]);
-
-	//Without magneto
-	//MadgwickAHRSupdateIMU(gyr_rate_rad[0], gyr_rate_rad[1], gyr_rate_rad[2], acc_G[0], acc_G[1], acc_G[2]);
-        MahonyAHRSupdateIMU(gyr_rate_rad[0], gyr_rate_rad[1], gyr_rate_rad[2], acc_G[0], acc_G[1], acc_G[2]);
-	computeAngles();
-
-	//printf("q0: %5.3f   q1: %5.3f   q2: %5.3f   q3: %5.3f\t", q0, q1, q2, q3); 
-	printf("Roll: %8.3f    Pitch: %8.3f    Yaw %8.3f\t", madAngles[0], madAngles[1], madAngles[2]);
-
-
-
-        //Each loop should be at least 20ms.
-        while(mymillis() - startInt < (DT*1000))
-        {
-            usleep(100);
-        }
-
-        printf("Loop Time %d\t", mymillis()- startInt);
-
-        //End with newline:
-        printf("\n");
-    }
+  // Local system variables
+  float norm; // vector norm
+  float SEqDot_omega_1, SEqDot_omega_2, SEqDot_omega_3, SEqDot_omega_4; // quaternion derrivative from gyroscopes elements
+  float f_1, f_2, f_3; // objective function elements
+  float J_11or24, J_12or23, J_13or22, J_14or21, J_32, J_33; // objective function Jacobian elements
+  float SEqHatDot_1, SEqHatDot_2, SEqHatDot_3, SEqHatDot_4; // estimated direction of the gyroscope error
+  // Axulirary variables to avoid reapeated calcualtions
+  float halfSEq_1 = 0.5f * SEq_1;
+  float halfSEq_2 = 0.5f * SEq_2;
+  float halfSEq_3 = 0.5f * SEq_3;
+  float halfSEq_4 = 0.5f * SEq_4;
+  float twoSEq_1 = 2.0f * SEq_1;
+  float twoSEq_2 = 2.0f * SEq_2;
+  float twoSEq_3 = 2.0f * SEq_3;
+  // Normalise the accelerometer measurement
+  norm = sqrt(a_x * a_x + a_y * a_y + a_z * a_z);
+  a_x /= norm;
+  a_y /= norm;
+  a_z /= norm;
+  // Compute the objective function and Jacobian
+  f_1 = twoSEq_2 * SEq_4 - twoSEq_1 * SEq_3 - a_x;
+  f_2 = twoSEq_1 * SEq_2 + twoSEq_3 * SEq_4 - a_y;
+  f_3 = 1.0f - twoSEq_2 * SEq_2 - twoSEq_3 * SEq_3 - a_z;
+  J_11or24 = twoSEq_3; // J_11 negated in matrix multiplication
+  J_12or23 = 2.0f * SEq_4;
+  J_13or22 = twoSEq_1; // J_12 negated in matrix multiplication
+  J_14or21 = twoSEq_2;
+  J_32 = 2.0f * J_14or21; // negated in matrix multiplication
+  J_33 = 2.0f * J_11or24; // negated in matrix multiplication
+  // Compute the gradient (matrix multiplication)
+  SEqHatDot_1 = J_14or21 * f_2 - J_11or24 * f_1;
+  SEqHatDot_2 = J_12or23 * f_1 + J_13or22 * f_2 - J_32 * f_3;
+  SEqHatDot_3 = J_12or23 * f_2 - J_33 * f_3 - J_13or22 * f_1;
+  SEqHatDot_4 = J_14or21 * f_1 + J_11or24 * f_2;
+  // Normalise the gradient
+  norm = sqrt(SEqHatDot_1 * SEqHatDot_1 + SEqHatDot_2 * SEqHatDot_2 + SEqHatDot_3 * SEqHatDot_3 + SEqHatDot_4 * SEqHatDot_4);
+  SEqHatDot_1 /= norm;
+  SEqHatDot_2 /= norm;
+  SEqHatDot_3 /= norm;
+  SEqHatDot_4 /= norm;
+  // Compute the quaternion derrivative measured by gyroscopes
+  SEqDot_omega_1 = -halfSEq_2 * w_x - halfSEq_3 * w_y - halfSEq_4 * w_z;
+  SEqDot_omega_2 = halfSEq_1 * w_x + halfSEq_3 * w_z - halfSEq_4 * w_y;
+  SEqDot_omega_3 = halfSEq_1 * w_y - halfSEq_2 * w_z + halfSEq_4 * w_x;
+  SEqDot_omega_4 = halfSEq_1 * w_z + halfSEq_2 * w_y - halfSEq_3 * w_x;
+  // Compute then integrate the estimated quaternion derrivative
+  SEq_1 += (SEqDot_omega_1 - (beta * SEqHatDot_1)) * deltat;
+  SEq_2 += (SEqDot_omega_2 - (beta * SEqHatDot_2)) * deltat;
+  SEq_3 += (SEqDot_omega_3 - (beta * SEqHatDot_3)) * deltat;
+  SEq_4 += (SEqDot_omega_4 - (beta * SEqHatDot_4)) * deltat;
+  // Normalise quaternion
+  norm = sqrt(SEq_1 * SEq_1 + SEq_2 * SEq_2 + SEq_3 * SEq_3 + SEq_4 * SEq_4);
+  SEq_1 /= norm;
+  SEq_2 /= norm;
+  SEq_3 /= norm;
+  SEq_4 /= norm;
 }
+
+
+void computeAngles()
+{
+  madAngles[0] = atan2f(SEq_1*SEq_2 + SEq_3*SEq_4, 0.5f - SEq_2*SEq_2 - SEq_3*SEq_3);
+  madAngles[1] = asinf(-2.0f * (SEq_2*SEq_4 - SEq_1*SEq_3));
+  madAngles[2] = atan2f(SEq_2*SEq_3 + SEq_1*SEq_4, 0.5f - SEq_3*SEq_3 - SEq_4*SEq_4);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
